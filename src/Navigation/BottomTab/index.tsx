d1491f2cbd48c012f71ad074c88cbb1d9e1f0e42
vitor-moraes <vitormoraes3@yahoo.com.br>
2020-12-17 21:37:17 -0300

    feat: bottom tab UI/UX

--- src/Navigation/BottomTab/index.tsx
+++ src/Navigation/BottomTab/index.tsx
@@ -1,99 +1,143 @@
-import * as React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
-import { Ionicons } from '@expo/vector-icons';
+import * as React from "react";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Ionicons } from "@expo/vector-icons";
+import { View, Text, TouchableOpacity, Alert } from "react-native";
 
-import Habits from "../../screens/Home/Habits"
-import Dailies from "../../screens/Home/Dailies"
-import ToDos from "../../screens/Home/ToDos"
-import Rewards from "../../screens/Home/Rewards"
+import {
+  Container,
+  Row,
+  TabBtn,
+  TextBtn,
+  TextBtnFocused,
+  AddBtn,
+  AddBtnBase,
+} from "./styles";
 
-import HeroHeader from '../../components/HeroHeader'
+import Habits from "../../screens/Home/Habits";
+import Dailies from "../../screens/Home/Dailies";
+import ToDos from "../../screens/Home/ToDos";
+import Rewards from "../../screens/Home/Rewards";
 
-const BottomTab = createBottomTabNavigator();
+import HeroHeader from "../../components/HeroHeader";
+
+const BottomTab = createMaterialTopTabNavigator();
 const TabStack = createStackNavigator();
 
- function BottomTabNavigator() {
+const TabBar = (props) => {
+  return (
+    <Container>
+      <Row>
+        <TabBtn onPress={() => props.navigation.navigate("Habits")}>
+          <Ionicons
+            name={props.state.index === 0 ? "add-circle" : "add-circle-outline"}
+            size={25}
+            color={props.state.index === 0 ? "#ffffff" : "#bbbbbb"}
+          />
+          {props.state.index === 0 ? (
+            <TextBtnFocused>Habits</TextBtnFocused>
+          ) : (
+            <TextBtn>Habits</TextBtn>
+          )}
+        </TabBtn>
+        <TabBtn onPress={() => props.navigation.navigate("Dailies")}>
+          <Ionicons
+            name={props.state.index === 1 ? "calendar" : "calendar-sharp"}
+            size={25}
+            color={props.state.index === 1 ? "#ffffff" : "#bbbbbb"}
+          />
+          {props.state.index === 1 ? (
+            <TextBtnFocused>Dailies</TextBtnFocused>
+          ) : (
+            <TextBtn>Dailies</TextBtn>
+          )}
+        </TabBtn>
+        <AddBtnBase>
+          <AddBtn
+            onPress={() =>
+              Alert.alert(
+                "Quase lá!",
+                "Aqui será adicionado o botão principal de adição de elemento",
+                [{ text: "Certo" }]
+              )
+            }
+          >
+            <Ionicons
+              style={{ transform: [{ rotate: "45deg" }] }}
+              name={"add-sharp"}
+              size={30}
+              color={"#ffffff"}
+            />
+          </AddBtn>
+        </AddBtnBase>
+        <TabBtn onPress={() => props.navigation.navigate("ToDos")}>
+          <Ionicons
+            name={
+              props.state.index === 2
+                ? "checkmark-circle"
+                : "checkmark-circle-outline"
+            }
+            size={25}
+            color={props.state.index === 2 ? "#ffffff" : "#bbbbbb"}
+          />
+          {props.state.index === 2 ? (
+            <TextBtnFocused>To Do's</TextBtnFocused>
+          ) : (
+            <TextBtn>To Do's</TextBtn>
+          )}
+        </TabBtn>
+        <TabBtn onPress={() => props.navigation.navigate("Rewards")}>
+          <Ionicons
+            name={props.state.index === 3 ? "ribbon" : "ribbon-outline"}
+            size={25}
+            color={props.state.index === 3 ? "#ffffff" : "#bbbbbb"}
+          />
+          {props.state.index === 3 ? (
+            <TextBtnFocused>Rewards</TextBtnFocused>
+          ) : (
+            <TextBtn>Rewards</TextBtn>
+          )}
+        </TabBtn>
+      </Row>
+    </Container>
+  );
+};
+
+function BottomTabNavigator() {
   return (
     <BottomTab.Navigator
       initialRouteName="TabOne"
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          if (route.name === 'Habits') {
-            return (
-              <Ionicons
-                name={
-                  focused
-                    ? 'add-circle'
-                    : 'add-circle-outline'
-                }
-                size={size}
-                color={color}
-              />
-            );
-          } else if (route.name === 'Dailies') {
-            return (
-              <Ionicons
-                name={focused ? 'calendar' : 'calendar-sharp'}
-                size={size}
-                color={color}
-              />
-            );
-          } else if (route.name === 'ToDos') {
-            return (
-              <Ionicons
-                name={focused ? 'checkmark-circle' : 'checkmark-circle-outline'}
-                size={size}
-                color={color}
-              />
-            );
-          } else if (route.name === 'Rewards') {
-            return (
-              <Ionicons
-                name={focused ? 'ribbon' : 'ribbon-outline'}
-                size={size}
-                color={color}
-              />
-            );
-          }
-        },
-      })}
-      >
-      <BottomTab.Screen
-        name="Habits"
-        component={Habits}
-        
-      />
-      <BottomTab.Screen
-        name="Dailies"
-        component={Dailies}
-      />
+      tabBarPosition="bottom"
+      tabBar={(props) => <TabBar {...props} />}
+    >
+      <BottomTab.Screen name="Habits" component={Habits} />
+      <BottomTab.Screen name="Dailies" component={Dailies} />
       <BottomTab.Screen
         name="ToDos"
         component={ToDos}
-        options={{title: "To Do's",}}
+        options={{ title: "To Do's" }}
       />
-      <BottomTab.Screen
-        name="Rewards"
-        component={Rewards}
-      />
+      <BottomTab.Screen name="Rewards" component={Rewards} />
     </BottomTab.Navigator>
   );
 }
 
 const BottomTabStack = () => {
-  return(
-    <TabStack.Navigator screenOptions={{
-      header: (props) => {
-        return(
-        <>
-          <HeroHeader {...props} />
-        </>
-        )},
-    }}>
+  return (
+    <TabStack.Navigator
+      screenOptions={{
+        header: (props) => {
+          return (
+            <>
+              <HeroHeader {...props} />
+            </>
+          );
+        },
+      }}
+    >
       <TabStack.Screen name="Tab" component={BottomTabNavigator} />
     </TabStack.Navigator>
-  )
-}
+  );
+};
 
-export default BottomTabStack
+export default BottomTabStack;
---
+++ src/Navigation/BottomTab/styles.tsx
@@ -0,0 +1,61 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  height: 50px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Row = styled.View`
+  flex-direction: row;
+  align-content: space-around;
+`;
+
+export const TabBtn = styled.TouchableOpacity`
+  background-color: #8400ff;
+  justify-content: center;
+  align-items: center;
+  height: 65px;
+  width: 75px;
+`;
+
+export const TextBtnFocused = styled.Text`
+  color: #ffffff;
+  font-size: 11px;
+`;
+
+export const TextBtn = styled.Text`
+  color: #bbbbbb;
+  font-size: 11px;
+`;
+
+export const AddBtn = styled.TouchableOpacity`
+  position: absolute;
+  transform: rotate(45deg);
+  background-color: #b96eff;
+  top: -58px;
+  margin-left: 15px;
+  margin-right: 15px;
+  border-radius: 5px;
+  padding-left: 2px;
+  justify-content: center;
+  align-items: center;
+  height: 45px;
+  width: 45px;
+`;
+
+export const AddBtnBase = styled.View`
+  top: 0px;
+  justify-content: center;
+  align-items: center;
+  height: 0px;
+  width: 0px;
+  border-left-width: 35px;
+  border-right-width: 35px;
+  border-bottom-width: 30px;
+  border-top-width: 35px;
+  border-left-color: #8400ff;
+  border-right-color:  #8400ff;
+  border-bottom-color:  #8400ff;
+  border-top-color: transparent;
+`;

1d5f1654109025a196c76d9e12150b7d47b17ade
vitor-moraes <vitormoraes3@yahoo.com.br>
2021-01-06 07:32:08 -0300

    Feat: toDos screen

--- src/screens/Home/ToDos/index.tsx
+++ src/screens/Home/ToDos/index.tsx
@@ -1,10 +1,186 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, Text, FlatList, ActivityIndicator, Alert } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
-import { Container } from './styles';
+import {
+  Container,
+  NullContainer,
+  NullBox,
+  NullIconBox,
+  NullTitle,
+  NullInfo,
+  CardsBox,
+  CardsBoxChecked,
+  CardsCheckBox,
+  CardsCheckMark,
+  CardsCheckBoxChecked,
+  CardsCheckMarkChecked,
+  CardsBody,
+  CardsBodyChecked,
+  CardsTitle,
+  CardsNotes,
+} from "./styles";
 
 const ToDos = () => {
-  return <Container><Text>To Do's</Text></Container>;
-}
+  const [loading, setLoading] = useState(true);
+  const [toDosData, setToDosData] = useState(null);
+  const [refrashing, setRefrashing] = useState(true);
+
+  const getToDosData = () => {
+    try {
+      setToDosData(testData);
+      setTimeout(() => {
+        setRefrashing(false);
+        setLoading(false);
+      }, 2000);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const testData = [
+    {
+      id: 1,
+      checked: false,
+      title: "Título 1",
+      notes: "Descrição 1",
+      difficulty: 3, // 1 to 4
+    },
+    {
+      id: 2,
+      checked: false,
+      title: "Título 2",
+      notes: "Descrição 2 UASHUSAA AIUSHUAH auahshsa  asiuh iaus as",
+      difficulty: 2, // 1 to 4
+    },
+    {
+      id: 3,
+      checked: false,
+      title: "Título 3",
+      notes:
+        "Descrição 3 haushuah asdhsa dhsasa as oaisoiashoiashdihas aiod haod hais dhoaishdoias asiodahs",
+      difficulty: 1, // 1 to 4
+    },
+    {
+      id: 4,
+      checked: true,
+      title: "Título 4",
+      notes: null,
+      difficulty: 1, // 1 to 4
+    },
+    {
+      id: 5,
+      checked: false,
+      title: "Título 5",
+      notes: null,
+      difficulty: 1, // 1 to 4
+    },
+    {
+      id: 6,
+      checked: true,
+      title: "Título 6",
+      notes: null,
+      difficulty: 1, // 1 to 4
+    },
+    {
+      id: 7,
+      checked: false,
+      title: "Título 7",
+      notes: null,
+      difficulty: 1, // 1 to 4
+    },
+    {
+      id: 8,
+      checked: true,
+      title: "Título 8",
+      notes: null,
+      difficulty: 1, // 1 to 4
+    },
+    {
+      id: 9,
+      checked: false,
+      title: "Título 9",
+      notes: null,
+      difficulty: 1, // 1 to 4
+    },
+    {
+      id: 10,
+      checked: false,
+      title: "Título 10",
+      notes: null,
+      difficulty: 1, // 1 to 4
+    },
+  ];
+
+  const renderItem = ({ item }) => {
+    return (
+      <>
+        {item.checked === false && (
+          <CardsBox key={item.id.toString()}>
+            <CardsCheckBox>
+              <CardsCheckMark />
+            </CardsCheckBox>
+            <CardsBody>
+              <CardsTitle>{item.title}</CardsTitle>
+              {item.notes !== null && <CardsNotes>{item.notes}</CardsNotes>}
+            </CardsBody>
+          </CardsBox>
+        )}
+      </>
+    );
+  };
+
+  useEffect(() => {
+    getToDosData();
+  }, []);
+
+  const handleRefrash = () => {
+    setRefrashing(true);
+    getToDosData();
+  };
+
+  return (
+    <>
+      {toDosData !== null && loading === false ? (
+        <Container>
+         <FlatList
+            data={toDosData}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id.toString()}
+            refreshing={refrashing}
+            onRefresh={() => handleRefrash()}
+          />
+        </Container>
+      ) : (
+        <>
+          {loading === true ? (
+            <NullContainer>
+              <NullBox>
+                <NullTitle>Carregando</NullTitle>
+              </NullBox>
+            </NullContainer>
+          ) : (
+            <NullContainer>
+              <NullBox>
+                <NullIconBox>
+                  <Ionicons
+                    name={"checkmark-circle-outline"}
+                    size={25}
+                    color={"#aaaaaa"}
+                  />
+                </NullIconBox>
+                <NullTitle>You don't have any To Do's</NullTitle>
+                <NullInfo>
+                  To Do's are task that only need to be completes once. Add
+                  checklist to your To Do's to incrise their value.
+                </NullInfo>
+              </NullBox>
+            </NullContainer>
+          )}
+        </>
+      )}
+    </>
+  );
+};
 
 export default ToDos;
--- src/screens/Home/ToDos/styles.tsx
+++ src/screens/Home/ToDos/styles.tsx
@@ -1,7 +1,132 @@
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
+import { Dimensions, PixelRatio } from "react-native";
+const { width, height } = Dimensions.get("window");
+const screenHeight = Math.round(PixelRatio.roundToNearestPixel(height));
 
 export const Container = styled.View`
-flex: 1;
+  padding-bottom: 10px;
+`;
+
+export const NullContainer = styled.View`
+  flex: 1;
+  background-color: #e2e2e2;
+  height: ${screenHeight}px;
   justify-content: center;
   align-items: center;
+`;
+
+export const NullBox = styled.View`
+  margin: 20px;
+  padding: 10px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const NullIconBox = styled.View``;
+
+export const NullTitle = styled.Text`
+  color: #8d8d8d;
+  font-size: 18px;
+  font-weight: bold;
+  margin: 2px;
+`;
+
+export const NullInfo = styled.Text`
+  color: #838383;
+  text-align: center;
+  line-height: 20px;
+`;
+
+export const CardsBox = styled.View`
+  background-color: #e2e2e2;
+  border-radius: 7px;
+  margin-left: 2.5%;
+  margin-right: 2.5%;
+  margin-top: 2%;
+  width: 95%;
+  height: auto;
+  min-height: 55px;
+  flex-direction: row;
+`;
+
+export const CardsBoxChecked = styled.View`
+  background-color: #e2e2e2;
+  border-radius: 7px;
+  margin-left: 2.5%;
+  margin-right: 2.5%;
+  margin-top: 2%;
+  width: 95%;
+  height: auto;
+  min-height: 55px;
+  flex-direction: row;
+`;
+
+export const CardsCheckBox = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #ffa631;
+  border-bottom-left-radius: 7px;
+  border-top-left-radius: 7px;
+  min-width: 40px;
+  max-width: 40px;
+  margin-right: 5px;
+  padding: 5px;
+`;
+
+export const CardsCheckBoxChecked = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #e2e2e2;
+  border-bottom-left-radius: 7px;
+  border-top-left-radius: 7px;
+  min-width: 40px;
+  max-width: 40px;
+  margin-right: 5px;
+  padding: 5px;
+`;
+
+export const CardsCheckMark = styled.View`
+  position: absolute;
+  background-color: #f7c37f;
+  border-radius: 15px;
+  height: 25px;
+  width: 25px;
+`;
+
+export const CardsCheckMarkChecked = styled.View`
+  position: absolute;
+  background-color: #d8d8d8;
+  border-radius: 5px;
+  height: 25px;
+  width: 25px;
+`;
+
+export const CardsBody = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: flex-start;
+  border-bottom-right-radius: 7px;
+  border-top-right-radius: 7px;
+  background-color: #e2e2e2;
+  padding: 10px;
+`;
+
+export const CardsBodyChecked = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: flex-start;
+  border-bottom-right-radius: 7px;
+  border-top-right-radius: 7px;
+  background-color: #e2e2e2;
+  padding: 10px;
+`;
+
+export const CardsTitle = styled.Text`
+  color: #000000;
+`;
+
+export const CardsNotes = styled.Text`
+  color: #8d8d8d;
 `;
